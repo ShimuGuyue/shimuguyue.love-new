0e58e99fbd001c04c7cb926e9ced729477f5c9f6
ShimuGuyue <shimuguyue513@qq.com>
2026-07-23 20:31:39 +0800

    Create 照片墙背景

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,8 +2,38 @@
 </script>
 
 <template>
-  <div class="home">
-    <h1>首页</h1>
-  </div>
+  <main class="home">
+    <div class="home__layout">
+      <div class="home__photo glass"></div>
+      <div class="home__info"></div>
+    </div>
+  </main>
 </template>
 
+<style scoped>
+.home {
+  padding: 24px;
+  height: calc(100vh - 80px);
+  overflow: hidden;
+}
+
+.home__layout {
+  height: 100%;
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  gap: 24px;
+}
+
+.home__photo {
+  min-height: 100%;
+}
+
+.home__info {
+  /* 右侧信息栏 */
+}
+</style>
+
+<style>
+@import "@/assets/glass.css";
+@import "@/assets/pink-theme.css";
+</style>
